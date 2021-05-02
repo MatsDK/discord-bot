3c6ca6626edd166b00d6888255650cd3e35270b3
matsdekort <matsdekort257@gmail.com>
2021-05-02 14:53:57 +0200

    clear roles

--- bot/commands/role.ts
+++ bot/commands/role.ts
@@ -9,50 +9,88 @@ export class CommandConstructor {
       cmdDetails,
       async (client: any, message: any, args: any[]) => {
         try {
-          const target = message.mentions.members.first();
-          if (!target) return message.reply("Please mention a user");
+          if (args[0]?.trim().toLowerCase() === "clear") {
+            if (!message.member.hasPermission("MANAGE_ROLES"))
+              return message.reply(
+                "You don't have persmission to remove roles"
+              );
 
-          if (target.id === message.author.id)
-            return message.reply("You can't role yourself");
+            if (!message.guild.me.hasPermission("MANAGE_ROLES"))
+              return message.reply("I don't have permission to remove roles");
 
-          if (!args[1])
-            return message.reply("Please provide a role name or id");
+            if (!args.slice(1).length)
+              return message.reply("Please mention roles you want to clear");
 
-          const roles: string[] = args
-            .slice(1)
-            .map((_: string) => _.toLowerCase());
-          const newRoles = message.guild.roles.cache.filter(
-            (_: any) =>
-              roles.includes(_.name.toLowerCase()) ||
-              roles.includes(_.id.toLowerCase())
-          );
+            const roles: string[] = args.slice(1);
+            const targetRoles = [
+              ...Array.from(message.mentions.roles).map(
+                ([key, role]: any) => role
+              ),
+              ...Array.from(
+                message.guild.roles.cache.filter((_: any) =>
+                  roles.includes(_.id.trim())
+                )
+              ).map(([key, role]: any) => role),
+            ].map((_: any) => _.id);
 
-          for (let [key, mentionedRole] of message.mentions.roles)
-            newRoles.set(key, mentionedRole);
+            const members = await message.guild.members.fetch(),
+              targetMembers = members.filter((_: any) =>
+                targetRoles.some((role) => _._roles.includes(role))
+              );
 
-          if (!message.member.hasPermission("MANAGE_ROLES"))
-            return message.reply("You don't have permissions to give roles");
+            for (let targetMember of targetMembers) {
+              let member = targetMember[1];
+              member.roles.remove(targetRoles).catch((err: any) => {
+                console.log(err);
+                return;
+              });
+            }
+          } else {
+            const target = message.mentions.members.first();
+            if (!target) return message.reply("Please mention a user");
 
-          const changed: { added: Array<string>; removed: Array<string> } = {
-            added: [],
-            removed: [],
-          };
+            if (target.id === message.author.id)
+              return message.reply("You can't role yourself");
 
-          for (let idx in Array.from(newRoles.entries())) {
-            const [_, role]: any = Array.from(newRoles.entries())[idx];
+            if (!args[1])
+              return message.reply("Please provide a role name or id");
 
-            if (
-              target.roles.cache.some(
-                (_: any) =>
-                  _.name.toLowerCase() === role.name.toLowerCase() ||
-                  _.id.toLowerCase() === role.name.toLowerCase()
-              )
-            ) {
-              await target.roles.remove(role);
-              changed.removed.push(role.name);
-            } else {
-              await target.roles.add(role);
-              changed.added.push(role.name);
+            const roles: string[] = args
+              .slice(1)
+              .map((_: string) => _.toLowerCase());
+            const newRoles = message.guild.roles.cache.filter(
+              (_: any) =>
+                roles.includes(_.name.toLowerCase()) ||
+                roles.includes(_.id.toLowerCase())
+            );
+
+            for (let [key, mentionedRole] of message.mentions.roles)
+              newRoles.set(key, mentionedRole);
+
+            if (!message.member.hasPermission("MANAGE_ROLES"))
+              return message.reply("You don't have permissions to give roles");
+
+            const changed: { added: Array<string>; removed: Array<string> } = {
+              added: [],
+              removed: [],
+            };
+
+            for (let idx in Array.from(newRoles.entries())) {
+              const [_, role]: any = Array.from(newRoles.entries())[idx];
+
+              if (
+                target.roles.cache.some(
+                  (_: any) =>
+                    _.name.toLowerCase() === role.name.toLowerCase() ||
+                    _.id.toLowerCase() === role.name.toLowerCase()
+                )
+              ) {
+                await target.roles.remove(role);
+                changed.removed.push(role.name);
+              } else {
+                await target.roles.add(role);
+                changed.added.push(role.name);
+              }
             }
           }
         } catch (err) {
